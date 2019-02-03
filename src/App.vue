4fa6d78d68290088f91cbd76d20aa092c6e2b5a2
Ana <ana.fandino@outlook.com>
2019-02-03 14:48:26 -0500

    main information

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="logoPosition">
+      <img style="height:30%;filter: brightness(0) invert(1); width: 30%" fluid alt="Ana logo" src="https://lh3.googleusercontent.com/y_iYAi-BIgoflpWT1E7JJr2w6S5ChSJPixmauN7BJ9NMTfw3SiodnFnqCLDJp_Xh0Y6cLOfzogtSZqUXELrCZc_KXoH2Vn3CnPmhOB5jbSPxHBFqqmN8cX_vyu1sDlVUx4Jer9QKtzD7bA0PM8ScQq5px55a55Kcj9QQjto7ANAhm7rRHPDqMck91WWIpSObeYIoyrhEWtXLFyhJEexztZT5T0eja5XKOwSBRblHVqjf1X--CmZBAZNJ9nyIlW3s_kmItD1JtVXIKzZzG-7Y8B9ddW-DO9WSgCx5ycpHVZxKenHtg-Xeyp83M32wdhIx6JNUpM-O2QN1AG7J7VcHi5XmCiMwD8SrHp3zANUCIy3bWEDYFygLebVAb4IZmKPVy2Pw6HYZLJeC3R-bY95mPBon3EFWZ_0Ffi8KoO4J2FOEdIAm91Xr6LK0k9Lqq4p0Ci6wXswZHYs-TQKjQhjhmGW-nmYFVKv36NgZrEO-P0tt0WM4SVKV2cvYhX4IdrxQvX_C4p5rrbUFwmZCu_Q1jYKTuzpdwIBOHKarOoHPPyQi8mmmoPeyK832fo47kM38jmd5VXKLmvRkJd2Lts0c_ZJbVNvCMgE4a0Cgz5IMyAekY2qJNEkzHrAue1YIY47zTI3qH6XRKmDw_HH3ClCmtA7k8o-u9qbd=w1024-h768-no">
+   </div>
+    <div class="logoPosition2">
+      <img style="height:30%; width: 30%" fluid alt="Ana logo" src="https://lh3.googleusercontent.com/y_iYAi-BIgoflpWT1E7JJr2w6S5ChSJPixmauN7BJ9NMTfw3SiodnFnqCLDJp_Xh0Y6cLOfzogtSZqUXELrCZc_KXoH2Vn3CnPmhOB5jbSPxHBFqqmN8cX_vyu1sDlVUx4Jer9QKtzD7bA0PM8ScQq5px55a55Kcj9QQjto7ANAhm7rRHPDqMck91WWIpSObeYIoyrhEWtXLFyhJEexztZT5T0eja5XKOwSBRblHVqjf1X--CmZBAZNJ9nyIlW3s_kmItD1JtVXIKzZzG-7Y8B9ddW-DO9WSgCx5ycpHVZxKenHtg-Xeyp83M32wdhIx6JNUpM-O2QN1AG7J7VcHi5XmCiMwD8SrHp3zANUCIy3bWEDYFygLebVAb4IZmKPVy2Pw6HYZLJeC3R-bY95mPBon3EFWZ_0Ffi8KoO4J2FOEdIAm91Xr6LK0k9Lqq4p0Ci6wXswZHYs-TQKjQhjhmGW-nmYFVKv36NgZrEO-P0tt0WM4SVKV2cvYhX4IdrxQvX_C4p5rrbUFwmZCu_Q1jYKTuzpdwIBOHKarOoHPPyQi8mmmoPeyK832fo47kM38jmd5VXKLmvRkJd2Lts0c_ZJbVNvCMgE4a0Cgz5IMyAekY2qJNEkzHrAue1YIY47zTI3qH6XRKmDw_HH3ClCmtA7k8o-u9qbd=w1024-h768-no">
+    </div>
+    <b-carousel id="carousel1"
+                style="text-shadow: 1px 1px 2px #333;"
+                controls
+                indicators
+                background="#ababab"
+                :interval="4000"
+                img-width="1024"
+                img-height="480"
+                v-model="slide"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
+    >
+
+      <!-- Text slides with image -->
+      <b-carousel-slide caption="First slide"
+                        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+                        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58">
+      </b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+             src="https://picsum.photos/1024/480/?image=55" alt="image slot">
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut pellentesque
+          ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+
+    </b-carousel>
+
+    <MyMainCV msg="Welcome to Your Vue.js App"/>
+    <b-card title="Design every aspect of your life"
+            footer-tag="footer">
+      <em slot="footer">Ana Camila Fandiño de la Hoz</em>
+    </b-card>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyMainCV from './components/MyMainCV.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MyMainCV
+  },
+  data () {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart () {
+      this.sliding = true
+    },
+    onSlideEnd () {
+      this.sliding = false
+    }
   }
 }
 </script>
 
-<style>
+<style scooped>
+  .alignLeft{
+    text-align: left;
+  }
+  .logoPosition {
+    text-align: left !important;
+    position: absolute;
+    top: 5% !important;
+    left: 0% !important;
+    z-index: 1;
+  }
+  .logoPosition2 {
+    text-align: left !important;
+    position: absolute;
+    top: 5% !important;
+    left: 0.2% !important;
+    z-index: 1;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/MyMainCV.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="hello">
+    <div>
+      <h1>Ana Camila Fandiño de la Hoz</h1>
+      <h3>Profile</h3>
+      <p>Estudiante de sexto semestre de Ingeniería de Sistemas en la Universidad de los Andes, bilingüe(inglés-español), amigable, cordial, competente para trabajar y dispuesta a aprender. En busca de puesto gratificante para comenzar prácticas profesionales para así aplicar el conocimiento ya obtenido durante la carrera.
+
+        Experiencia como asistente de investigación en la Universidad de Wisconsin-Milwaukee en proyecto de Inteligencia Artificial que le permitieron obtener conocimientos de minería de datos, Watson, apis(Twitter y Reddit), IUI(Intelligence User Interface) y NLP(Natural Language Processing).
+
+        Asimismo, habilidades en programación y lenguajes, destacados en la participación en varias maratones de programación durante el año pasado incluyendo competencias nacionales y regionales del CCPL(Colombian Collegiate Programming League).
+
+        Adicional, de experiencia laboral 8 meses como agente bilingüe de call center en Sitel que le permitieron desarrollar habilidades como el buen trato con el cliente.
+      </p>
+      <h3>Goal</h3>
+      <p>
+        Sixth semester student of Computer and System Engineering and Design at Los Andes University in Bogotá, Colombia.
+        Friendly, gentle, kind and excited to learn. Looking for a research project in another country where to practice
+        english skills and develop skills related to the career. Good scores on english tests such as TOELF,
+        ICFES and CERF. Additional to this, participation on programming competitions on the Colombian Collegiate
+        Programming League. Other experience include some experience in customer service working part time at a
+        bilingual call center.
+        Sixth semester student of Computer and System Engineering at Los Andes University in Bogotá, Colombia. Friendly, gentle, kind and anxious to learn. Looking for a research project in another country where to practice english skills and develop skills related to the career. Good scores on english tests such as TOELF, ICFES and CERF. Additional to this, participation on programming competitions on the Colombian Collegiate Programming League. Other experience include some experience in customer service working part time in an english call center.
+      </p>
+      <h3>Goal</h3>
+      <p>En busca de un puesto gratificante para comenzar prácticas profesionales.</p>
+      <h3>Education</h3>
+      <p>
+        High School at Gimnasio Vermont							2013
+        University at Los Andes University (1 year from graduation)	      	       		2013-now
+      </p>
+      <h3>Job Experience</h3>
+      <p>
+        Part time call center agent on Sitel(for 8 months)					2016
+        Asistente de Investigación en Universidad de Wisconsin-Milwaukee(5 meses)		2017
+      </p>
+      <h3>Skills</h3>
+      <p>
+        Knowledge: Frontend, backend, database, Http requests, programming languages, servers
+        Tools: Java, JavaScript, HTML, CSS, C++, SQLDeveloper, Assembly,Tomcat, Glassfish, Maven, Github
+        Other Participations: Neuro-Linguistic Psychology seminar, Wiring Challenge and CCPL programming contests.
+
+      </p>
+      <h3>Hobbies</h3>
+      <p>Tenis, squash, trotar</p>
+      <p>
+        GitHub https://github.com/acfandino10
+        Email ac.fandino10@uniandes.edu.co
+        Dirección Cra 54D # 183-50
+        Teléfono +57 3195320954
+        Bogotá D.C, Colombia
+      </p>
+
+    </div>
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
